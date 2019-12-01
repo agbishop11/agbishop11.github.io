185d3e2cc36d967f33547791c2a3085ae90b39e5
Aaron Bishop <aaronbis@andrew.cmu.edu>
2019-12-01 10:57:55 -0500

    Create script.js

---
+++ Grey_Matter/JS/script.js
@@ -0,0 +1,76 @@
+//Preloader
+$(window).on("load", function () {
+    $("#loading").delay(500).fadeOut('slow');
+    $("#curr-sts").delay(100).fadeOut();
+});
+
+
+//Nav Bar
+
+$(function () {
+    
+    $(window).scroll(function() {
+        
+        if( $(window).scrollTop() > 200 ) {
+            
+            $("nav").addClass("nav-background");
+            
+            $(".navbar-brand img").attr("src", "IMG/grey-matter-images/Lotus_1.png")
+        } else {
+            
+            $("nav").removeClass("nav-background");
+            
+            $(".navbar-brand img").attr("src", "IMG/grey-matter-images/Lotus_light.png")
+        }
+        
+    })
+    
+})
+
+/* Responsive Tabs */
+$(function () {
+
+    $("#services-books").responsiveTabs({
+        animation: 'slide',
+
+    });
+
+});
+
+/*Work Section*/
+
+
+$(window).on("load", function () {
+
+    $("#animation-container").isotope({
+
+    });
+
+    $(".iso-fil").on("click", "button", function () {
+
+
+        let selection = $(this).attr("data-filter");
+
+        $("#animation-container").isotope({
+            filter: selection
+        });
+
+
+        $(".iso-fil").find(".active").removeClass("active");
+
+        $(this).addClass('active');
+
+
+    });
+
+});
+
+$(function () {
+
+    $("#work-container").magnificPopup({
+        delegate: "a",
+        type: "image"
+
+    });
+
+});
